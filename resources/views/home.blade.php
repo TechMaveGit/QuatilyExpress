a616bc4232536c6c63ede30368ae45e6bf36ce1b
TechMaveGit <130988864+basantTechMave@users.noreply.github.com>
2024-05-24 11:38:14 +0530

    url remove from all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <h1 class="page-title">Dashboard</h1>
     <div>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('home_page') }}">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
         </ol>
     </div>
